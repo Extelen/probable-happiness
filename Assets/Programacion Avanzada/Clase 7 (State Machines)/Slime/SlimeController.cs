65a1f3d5d81cc60fc0cdf05a8807840edd8b355a
Rodrigo Saguer Pacheco <extelendev@gmail.com>
2024-10-18 19:17:43 -0300

    Async, Await y IEnumerator Start

--- Assets/Programacion Avanzada/Clase 7 (State Machines)/Slime/SlimeController.cs
+++ Assets/Programacion Avanzada/Clase 7 (State Machines)/Slime/SlimeController.cs
@@ -16,6 +16,16 @@ public class SlimeController : MonoBehaviour
         stateMachine.SwitchState(states.idle);
     }
 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Pasaron 3 segundos");
+    }
+
     public void GetDamage()
     {
         Debug.Log("Solo tengo un punto de vida asi que me mori");
--- Assets/Programacion Avanzada/Clase 7 (State Machines)/Slime/SlimeState_Idle.cs
+++ Assets/Programacion Avanzada/Clase 7 (State Machines)/Slime/SlimeState_Idle.cs
@@ -2,34 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Threading.Tasks;
+using System;
+
 [System.Serializable]
 public class SlimeState_Idle : SlimeStateBase
 {
     public float timeToWalk;
 
-    private float currentTime;
-
-    public override void Enter()
+    public override async void Enter()
     {
         base.Enter();
-        currentTime = timeToWalk;
         Debug.Log("Me quede quieto");
 
+
+        Debug.Log("Voy a empezar a esperar");
+        // TimeSpan waitSeconds = new TimeSpan(0, 0, timeToWalk);
+        // await Task.Delay(waitSeconds);
+
+        await Task.Delay((int)(timeToWalk * 1000));
+
+        await Task.Yield();
+        Debug.Log("Termine de esperar y me voy a patrol");
+
+        // Controller.stateMachine.SwitchState(Controller.states.patrol);
     }
 
     public override void Logic()
     {
         base.Logic();
 
-        if (DetectPlayer())
-        {
-            Controller.stateMachine.SwitchState(Controller.states.followPlayer);
-        }
+        // if (DetectPlayer())
+        // {
+        // Controller.stateMachine.SwitchState(Controller.states.followPlayer);
+        // }
 
-        currentTime -= Time.deltaTime;
-        if (currentTime <= 0)
-        {
-            Controller.stateMachine.SwitchState(Controller.states.patrol);
-        }
     }
 }
